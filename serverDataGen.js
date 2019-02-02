df8783d5cf786d7c5d0e7cc41494c91fbf7490c3
jsoulis <jtsoulis7@gmail.com>
2019-02-02 15:03:46 +0900

    serverDataGen.js contains most of the new implementation for trying to generate large PI Data set... It is much faster at writing to file for large amount of data, but not satisfied yet..

--- serverDataGen.js
+++ serverDataGen.js
@@ -1,40 +1,129 @@
 let fs = require('fs');
+//SET THESE PARAMETERS
+let NUMBER_OF_TAGS = 10;
+let TAG_NAME_TEMP = 'Tag_';
+let START_TIME = '30-Jan-18 08:00:00';
+let END_TIME = '31-Jan-18 08:00:00';
+let SCAN_FREQ = 1; //number of seconds
+let MAX_SECTION_SIZE = 100000; //max number of elements in memory before writing to file
 
-try {
-    let input = "15-Jul-18 09:36:28.000"
-    let limit = 31000000; //about the num of seconds in a year
 
-    //this took forever and the file grew at a very slow pace while CPU usage exploded.
-    //I think I should write to a buffer first and then periodically append
-    //what is written to the buffer, to the file.
-  /*   for (let i = 0; i < limit; i++) {
-        fs.appendFile('message.txt', input);
-    } */
-    let segment = limit / 10000;
-    let buffer = '';
-    for (let i = 0; i < segment; i++) {
-        for (let j = 0; j < 10000; j++) {
-            buffer += input;
-        }
-        fs.appendFileSync('message.txt', buffer);
-        buffer = '';
+//calculate how many times to make fs.appendFileSync call per tag
+const numSections = (startTime, endTime, scanFreqSecs, maxSectionSize) => {
+    let UTCStartTime = Date.parse(startTime);
+    let UTCEndTime = Date.parse(endTime);
+
+    //only considering scanFreqsecs of 1... other values require addressing corner cases
+    let timeStep = 1000 * scanFreqSecs;
+    let numTimeStamps = (UTCEndTime - UTCStartTime) / timeStep;
+
+    let sections = Math.floor(numTimeStamps / maxSectionSize);
+    let remains = numTimeStamps % maxSectionSize;
+
+    chunkInfo = {
+        remains: remains,
+        sections: sections
     }
-    console.log('The "data to append" was appended to file!');
-} catch (err) {
-    /* Handle the error */
+
+    //31,536,000
+
+    return chunkInfo;
 }
 
+const UTCToPITime = (UTCTime) => {
+    let PITimeStamp = '';
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+ 
+    let date = new Date(UTCTime);
+    let day = date.getDate();
+    let month = months[date.getMonth()];
+    let year = date.getFullYear().toString().substring(2);
+    let hour = date.getHours().toString().length > 1 ? date.getHours().toString() : '0' + date.getHours().toString();
+    let min = date.getMinutes().toString().length > 1 ? date.getMinutes().toString() : '0' + date.getMinutes().toString();
+    let sec = date.getSeconds().toString().length > 1 ? date.getSeconds().toString() : '0' + date.getSeconds().toString();
+    PITimeStamp = day + "-" + month + "-" + year + " " + hour + ":" + min + ":" + sec;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+    return PITimeStamp;
+}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const generateValue = () => {
+    let data = Math.floor(Math.random()*1000);
+    return data;
+}
+
+const createChunk = (startTime, endTime, tagName) => {
+    let chunck = '';
+
+    let line = '';
+    let value = '';
+    let pitime = '';
+
+    let utcTime = startTime;
+    let timeIncr = SCAN_FREQ * 1000;
+    let numTags = 1 + (endTime - startTime) / 1000;
+
+    for (let i = 0; i < numTags; i++) {
+        pitime = UTCToPITime(utcTime);
+        value = generateValue();
+        line = tagName + ',' + pitime + ',' + value + '\n';
+        chunck += line;
+        utcTime += timeIncr;
+    }
+
+    return chunck;
+
+}
+
+const getTagName = (tagNumber) => {
+    return TAG_NAME_TEMP + tagNumber;
+}
+
+const generateFile = (startTime, endTime, scanFreqSecs, numTags, maxSectionSize) => {
+
+    const block = numSections(startTime, endTime, scanFreqSecs, maxSectionSize);
+    let tagName = '';
+    let dataChunck = '';
+    let UTCStartTime;
+    let UTCEndTime;
+
+    //outer loop run for each tag
+    for (let i = 0; i < numTags; i++) {
+        UTCStartTime = Date.parse(startTime);
+        tagName = getTagName(i+1);
+        //console.log("tag", tagName);
+
+        //console.log("block.sections", block.sections);
+
+        //handle sections
+        for (let j = 0; j < block.sections; j++) {
+
+            //generate chunks of MAX_SECTION_SIZE
+            UTCEndTime = UTCStartTime + maxSectionSize*1000 - 1000; //sections are 1 sec based...need milisec
+            //console.log(`Section: ${j} - start: ${UTCStartTime}  end: ${UTCEndTime}`);
+            dataChunck = createChunk(UTCStartTime, UTCEndTime, tagName);
+            UTCStartTime = UTCEndTime + 1000;
+
+            if(j === block.sections - 2) {
+                UTCStartTime += 1000; //hacky way to include endTime... ugh not beautiful at all
+            }
+            //write To File
+            fs.appendFileSync('message.txt', dataChunck);
+        }
+
+        //handle remains
+        UTCStartTime = Date.parse(startTime) + maxSectionSize * block.sections * 1000 + 1000;
+        UTCEndTime = UTCStartTime + block.remains * 1000 - 1000;
+        //console.log(`Sta Time For Remains: ${UTCStartTime}`);
+        //console.log(`End Time for Remains: ${UTCEndTime}`);
+
+        dataChunck = createChunk(UTCStartTime, UTCEndTime, tagName);
+        fs.appendFileSync('message.txt', dataChunck);
+
+    }
+}
+
+generateFile(START_TIME, END_TIME, SCAN_FREQ, NUMBER_OF_TAGS, MAX_SECTION_SIZE);
+
+
+
